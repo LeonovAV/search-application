2260cf04f378ffaf056dd9d3de9ed38b1442083e
anleonov <anton.leonov@clausematch.com>
2021-05-24 00:16:44 +0300

    Add simple ngram tokenizer
    In this commit we add ngram tokenizer with lowercased tokens.

--- document-index/src/main/kotlin/com/anleonov/index/tokenizer/NGramTokenizer.kt
+++ document-index/src/main/kotlin/com/anleonov/index/tokenizer/NGramTokenizer.kt
@@ -2,11 +2,35 @@ package com.anleonov.index.tokenizer
 
 import com.anleonov.index.api.Token
 import com.anleonov.index.api.Tokenizer
+import org.slf4j.LoggerFactory
 
-class NGramTokenizer : Tokenizer {
+class NGramTokenizer(
+    private val nGramSize: Int
+) : Tokenizer {
+
+    private val logger = LoggerFactory.getLogger(NGramTokenizer::class.java)
 
     override fun tokenize(content: String): List<Token> {
-        return listOf(Token(content))
+        if (content.isEmpty()) return emptyList()
+
+        if (content.length <= nGramSize) return listOf(Token(content))
+
+        val tokens = mutableListOf<Token>()
+        for (i in 0..content.length - nGramSize) {
+            val tokenContent = prepareTokenContent(content, i)
+            tokens.add(Token(tokenContent))
+        }
+
+        logger.trace("Extracted tokens: $tokens")
+
+        return tokens
+    }
+
+    private fun prepareTokenContent(content: String, currentPosition: Int): String {
+        return content.substring(
+            startIndex = currentPosition,
+            endIndex = currentPosition + nGramSize
+        ).toLowerCase()
     }
 
 }
---
+++ document-index/src/test/kotlin/com/anleonov/index/tokenizer/NGramTokenizerTest.kt
@@ -0,0 +1,57 @@
+package com.anleonov.index.tokenizer
+
+import com.anleonov.index.api.Token
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class NGramTokenizerTest {
+
+    private val triGramTokenizer = NGramTokenizer(nGramSize = 3)
+
+    @Test
+    fun `should not create tokens for empty content`() {
+        val tokens = triGramTokenizer.tokenize("")
+        assertTrue(tokens.isEmpty())
+    }
+
+    @Test
+    fun `should create one token with two characters`() {
+        val tokens = triGramTokenizer.tokenize("ab")
+
+        val expectedTokens = listOf(
+            Token("ab")
+        )
+        assertEquals(expectedTokens, tokens)
+    }
+
+    @Test
+    fun `should create two tokens with the same length`() {
+        val tokens = triGramTokenizer.tokenize("abcd")
+
+        val expectedTokens = listOf(
+            Token("abc"),
+            Token("bcd")
+        )
+        assertEquals(expectedTokens, tokens)
+    }
+
+    @Test
+    fun `should create several tokens in lowercase with space`() {
+        val tokens = triGramTokenizer.tokenize("Hello World")
+
+        val expectedTokens = listOf(
+            Token("hel"),
+            Token("ell"),
+            Token("llo"),
+            Token("lo "),
+            Token("o w"),
+            Token(" wo"),
+            Token("wor"),
+            Token("orl"),
+            Token("rld")
+        )
+        assertEquals(expectedTokens, tokens)
+    }
+
+}
